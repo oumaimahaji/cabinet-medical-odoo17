f9143a85207013972208478415d2ebfb666b426c
Oumaima Hajji <oumaima@example.com>
2026-09-11 18:58:06 +0100

    chore(release): bump cabinet_medical version to 17.0.1.0.8

--- custom_addons/cabinet_medical/__manifest__.py
+++ custom_addons/cabinet_medical/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Cabinet Médical',
-    'version': '17.0.1.0.7',
+    'version': '17.0.1.0.8',
     'summary': 'Gestion d un cabinet médical',
     'author': 'Ton Nom',
     'category': 'Healthcare',
