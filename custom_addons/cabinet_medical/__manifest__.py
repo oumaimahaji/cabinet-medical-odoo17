16a27d578fa9037631cd684c552bfaa0cac7eed7
Oumaima Hajji <oumaima@example.com>
2026-09-12 20:35:11 +0100

    style(rendezvous): align rendezvous list view design, typography and header layout with patients list

--- custom_addons/cabinet_medical/__manifest__.py
+++ custom_addons/cabinet_medical/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Cabinet Médical',
-    'version': '17.0.1.0.17',
+    'version': '17.0.1.0.18',
     'summary': 'Gestion d un cabinet médical',
     'author': 'Ton Nom',
     'category': 'Healthcare',
