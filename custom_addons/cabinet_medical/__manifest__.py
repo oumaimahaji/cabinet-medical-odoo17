994dd9e07ed2dcddb58be6638a1c0c2efd4e0c19
Oumaima Hajji <oumaima@example.com>
2026-09-12 20:57:45 +0100

    refactor(views): remove cnam validity column from patient list view

--- custom_addons/cabinet_medical/__manifest__.py
+++ custom_addons/cabinet_medical/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Cabinet Médical',
-    'version': '17.0.1.0.21',
+    'version': '17.0.1.0.22',
     'summary': 'Gestion d un cabinet médical',
     'author': 'Ton Nom',
     'category': 'Healthcare',
